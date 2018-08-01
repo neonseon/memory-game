9240ec5c13f779cd075a02f8748742680a0da29e
seon- <skcirs@gmail.com>
2018-07-31 17:02:14 -0700

    feat: Update starting point files to make an interactive Memory Game with JS

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Memory Game Project
 
+This is a memory game consisting of 16 cards a user has to match in pairs. The game assigns a star rating based on performance, times the game, and the number of moves required to match all cards. For more detail, view the instructions below.
+
 ## Table of Contents
 
 * [Instructions](#instructions)
@@ -7,14 +9,33 @@
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+### Memory Game Logic
 
-To get started, open `js/app.js` and start building out the app's functionality
+The game randomly shuffles the cards. A user wins once all cards have successfully been matched.
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+### Star Rating
+
+The game displays a star rating (from 1 to 3) that reflects the player's performance. At the beginning of a game, it displays 3 stars. After some number of moves, it changes to a lower star rating. After a few more moves, it changes to a even lower star rating (down to 1).
+
+### Move Counter
+
+Game displays the current number of moves a user has made.
+
+### Timer
+
+When the player starts a game, a displayed timer also starts. Once the player wins the game, the timer stops.
+
+### Restart Button
+
+A restart button allows the player to reset the game board, the timer, moves, and the star rating.
+
+### Congratulations Popup
+
+When a user wins the game, a modal appears to congratulate the player and ask if he/she wants to play again. It also tells the user how much time it took to win the game, the number of moves, and what the star rating was.
+
 
 ## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+This project was created for the FEND Udacity course. Therefore, we most likely will not accept pull requests.
 
 For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
-
+/*
+ * Key variables
+ */
 
 let moves = 0;
-const stars = 3;
-const startTime = 0;
-const endingTime = 0;
-var flippedCards = [];
+let starRating = 3;
+let minutes = 0;
+let seconds = 0;
+let flippedCards = [];
+let pairedMatches = [];
 
 /*
  * Create a list that holds all of your cards
  */
-const cards = document.body.firstChild.nextElementSibling.childNodes[5].getElementsByTagName('i');
+
+let deck = document.querySelector('.deck');
+let cards = deck.getElementsByTagName('i');
+
+//document.querySelector('.deck').querySelectorAll('i') returns nodelist
 
 /*
  * Display the cards on the page
@@ -17,6 +24,12 @@ const cards = document.body.firstChild.nextElementSibling.childNodes[5].getEleme
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+// Grab current order of cards established in HTML
+const cardArray = [];
+ for (let card of cards) {
+		cardArray.push(card.className);
+	}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -33,14 +46,13 @@ function shuffle(array) {
     return array;
 }
 
-// All cards start flipped over
-function begin() {
-	for (const card of cards) {
-		card.parentNode.className = 'card';
-	}	
-}
 
-begin();
+// Take the shuffled order of cards and build the HTML for the cards
+function buildHTML(array) {
+  for (let i = 0; i < array.length; i++) {
+  	deck.children[i].innerHTML = '<i class="' + array[i] + '"></i>';
+  }
+}
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -53,55 +65,161 @@ begin();
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-const deck = document.body.firstChild.nextElementSibling.childNodes[5];
+const stars = document.querySelector('.stars');
+const moveContainer = document.querySelector('.moves');
+const restart = document.querySelector('.restart');
 
-deck.addEventListener('click', function() {
-	flipOverCard();
-	checkCards();
-	moves++;
-	console.log(event.target);
+// User presses restart, this function will refresh the game.
+restart.addEventListener('click', function() {
+	clearTimeout(time);
+	begin();
 })
 
+// This function calls other functions to flip over the card, check the cards, capture and update the moves, and adjust stars based on performance
+deck.addEventListener('click', function() {
+	flipOverCard();
+	updateMoves();
+	starAdjustment();
+})
+
+// This function first makes sure that the LI is targeted and that it is not performed on a card that is already matched/turned over. It then adds the flipped cards to the flippedCards array. Flipped over cards should match the number of moves.
 function flipOverCard() {
 	if (event.target.tagName == 'LI' && event.target.className != 'card match') { // nothing happens if user clicks the icon instead of the card
 		event.target.className = 'card open show';
-		flippedCards.push(event.target.childNodes[1]);
+		moves++; // with every qualified flip, the move is captured - not captured if they click an open matched card by accident
+		flippedCards.push(event.target.firstElementChild);
+		checkCards();
 	}
 }
 
+// This function checks the cards only if two have been flipped over. It compares the class of the I tags last two cards in the flippedCards array to determine a match. If there is a match, a new class is assigned them to turn them green, and they are pushed to the pairedMatches array and the function is directed to run the cardsMatch function, otherwise it will run the flipMismatch function.
 function checkCards() {
 	if (flippedCards.length >= 2 && flippedCards.length % 2 == 0) { // doesn't start working until there are at least 2 cards and only attempts matching on pairs
 		if (flippedCards[flippedCards.length-1].className === flippedCards[flippedCards.length-2].className) {
 			flippedCards[flippedCards.length-1].parentElement.className = 'card match';
 			flippedCards[flippedCards.length-2].parentElement.className = 'card match';
+			pairedMatches.push(flippedCards[flippedCards.length-1]);
+			pairedMatches.push(flippedCards[flippedCards.length-2]);
+			setTimeout(function(){ cardsMatch(); }, 500);
 		} else {
 			flippedCards[flippedCards.length-1].parentElement.className = 'card mismatch';
 			flippedCards[flippedCards.length-2].parentElement.className = 'card mismatch';
-			setTimeout(function(){ flipMismatch(); }, 1000);				
+			setTimeout(function(){ flipMismatch(); }, 500);
 		}
-	} 
-	if (flippedCards.length == 16) {
-		// stop timer
-		// popup module with congrats and play again button
-		// get star rating
-		// get number of moves/clicks
-		//setTimeout(function(){ alert('Congratulations!'); }, 3000);
-		alert('Congratulations!');
 	}
 }
 
+// This function will begin once all 16 cards are matched.
+function cardsMatch() {
+		if (pairedMatches.length == 16) {
+		clearTimeout(time); // stop timer
+		// popup module with congrats and play again button, get star rating, get number of moves/clicks
+		modal.style.display = 'block';
+		document.querySelector('.modal-content p').innerHTML = 'CONGRATULATIONS!<p>You won in ' + moves + ' moves with ' + starRating + ' stars and a time of ' + mins + ':' + ((seconds < 10) ? '0' + seconds : seconds) + '!</p>';
+		const btn = document.getElementById('playAgain'); // When the user clicks on the button, they can play again and the modal will disappear
+		btn.onclick = function() {
+	    modal.style.display = 'none';
+	    begin();
+		}
+	}
+}
+
+// If the two last cards flipped over do not match, they are turned back over.
 function flipMismatch() {
 	flippedCards[flippedCards.length-1].parentElement.className = 'card';
-	flippedCards[flippedCards.length-2].parentElement.className = 'card';	
-	flippedCards.pop();
-	flippedCards.pop();	
+	flippedCards[flippedCards.length-2].parentElement.className = 'card';
 }
 
-function starRating() {
-	
+// This function determines how the user's performance will be rated based on their number of moves halfway through all matches, and at all matches.
+function starAdjustment() {
+	if ((moves > 18 && pairedMatches.length < 8) || (moves > 32 && pairedMatches.length < 16)) {
+		stars.lastElementChild.firstElementChild.className = 'fa fa-star-o';
+		starRating = 2;
+	}
+	if ((moves > 22 && pairedMatches.length < 8) || (moves > 36 && pairedMatches.length < 16)) {
+		stars.childNodes[3].firstElementChild.className = 'fa fa-star-o';
+		starRating = 1;
+	}
 }
 
-//restart
-document.getElementsByClassName('restart').addEventListener('click', function() {
-	begin();
-})
+// This function updates the moves in the game and doesn't start the timer until the first move.
+function updateMoves() {
+	moveContainer.textContent = moves;
+  if (moves == 1) {
+	startTimer();
+	}
+}
+
+// When the user presses reset, the timer is set back to zero
+function reset() {
+  mins = 0;
+  seconds = 0;
+  document.querySelector('#mins').textContent = '00:';
+  document.querySelector('#seconds').textContent = '00';
+}
+
+//
+function startTimer(){
+  time = setTimeout(function() {
+    seconds++;
+
+    if(seconds > 59) {
+    	seconds = 0;
+    	mins++;
+	  }
+
+	  document.querySelector('#mins').textContent = mins + ':';
+	  document.querySelector('#seconds').textContent = seconds;
+
+    if(mins < 10) {
+      document.querySelector('#mins').textContent ='0'+ mins + ':';
+    } else {
+			document.querySelector('#mins').textContent = mins + ':';
+		}
+
+    if(seconds < 10) {
+      document.querySelector('#seconds').textContent = '0' + seconds;
+    } else {
+      document.querySelector('#seconds').textContent = seconds;
+    }
+
+    startTimer();
+  }, 1000);
+}
+
+// Get the modal that pops up once the user matches all cards
+const modal = document.getElementById('winModal');
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = 'none';
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+      modal.style.display = 'none';
+  }
+}
+
+// This function begins the game.
+function begin() {
+	for (const card of cards) {
+		card.parentNode.className = 'card'; // All cards start flipped over
+	}
+	shuffle(cardArray); // Shuffle the cards
+	buildHTML(cardArray); // Build the HTML for the cards
+	moves = 0; // reset moves
+	pairedMatches = []; // empty array
+	flippedCards = []; // empty array
+	updateMoves(); // keep track of moves
+	starRating = 3; // start with 3 starts
+	stars.lastElementChild.firstElementChild.className = 'fa fa-star';
+	stars.childNodes[3].firstElementChild.className = 'fa fa-star';
+	reset();
+}
+
+begin();
